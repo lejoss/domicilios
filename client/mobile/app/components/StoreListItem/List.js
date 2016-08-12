3172218cc5d2221bdcc7e4f4cbe566674db88904
lejoss <nativo.alejandro@gmail.com>
2016-08-11 20:36:52 -0500

    added check template

--- client/mobile/app/components/StoreListItem/List.js
+++ client/mobile/app/components/StoreListItem/List.js
@@ -6,12 +6,14 @@ import {Text, View} from 'react-native'
 import TabView from 'react-native-scrollable-tab-view'
 import StoreList from './index'
 import ListDetail from './ListDetail'
+import Check from './Check'
 
 const List = () => (
     <View style={{flex:1}}>
         <TabView>
             <StoreList tabLabel="list" />
             <ListDetail tabLabel="detail" />
+            <Check tabLabel="check" />
         </TabView>
     </View>
 )
